a9691ad0bcf4e9a5e61a991621110a68bf31993a
Peteroyanoghafo <68973790+Peteroyanoghafo@users.noreply.github.com>
2022-09-08 01:25:49 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -5,3 +5,40 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
 });
+
+var header = document.getElementsByClassName("nav_bar");
+for (var i = 0; i < nav_links.length; i++) {
+    btns[i].addEventListener("click",function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className =
+        current[0].className.replace ("active", "");
+        this.className += " active";
+    });
+}
+
+
+// function myFunction() {
+//     var input, filter, title, txtValue;
+//     input = document.getElementById ("myInput");
+//     filter = input.value.toUpperCase();
+//     title = document.getElementsByClassName("job_title");
+//     txtValue = title.textContent || title.innerText;
+//         if(txtValue.toUpperCase().indexOf(filter) > -1) {
+            
+
+
+
+//         }
+// }
+function myFunction() {
+     var searchValue = document.getElementById("myInput").value;
+     if (searchValue === "job") {
+        window.open("/latestJobs.html");
+     }
+     else if (searchValue === "latest") {
+        window.open("/latestJobs.html#moreJobs")
+     }
+     else{
+       window.open("/latestJobs.html")
+     }
+}
